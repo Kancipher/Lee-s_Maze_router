b97a639842a0f466e173b6441d470490745b3e38
Mohamed Alaaeldin Ayoub <98830324+mohamedayoub110@users.noreply.github.com>
2025-05-21 00:27:08 +0300

    Update binding.cpp

--- src/binding.cpp
+++ src/binding.cpp
@@ -1,17 +1,17 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include "functions.hpp"
-
-namespace py = pybind11;
-
-PYBIND11_MODULE(routing, m) {
-    m.def("readfile", &readfile, "Read input file and run routing");
-
-    m.def("get_grid", []() {
-        return grid;
-    }, "Get the current routing grid");
-
-    m.def("get_net_name_grid", []() {
-        return net_name_grid;
-    }, "Get the net name grid for visualization");
-}
+//#include <pybind11/pybind11.h>
+//#include <pybind11/stl.h>
+//#include "functions.hpp"
+//
+//namespace py = pybind11;
+//
+//PYBIND11_MODULE(routing, m) {
+//    m.def("readfile", &readfile, "Read input file and run routing");
+//
+//    m.def("get_grid", []() {
+//        return grid;
+//    }, "Get the current routing grid");
+//
+//    m.def("get_net_name_grid", []() {
+//        return net_name_grid;
+//    }, "Get the net name grid for visualization");
+//}
